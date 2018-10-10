0eb2ee868b1f3cdb7667f8b30550d74b7f6b9c54
Cedric David <cedric.david@btopenworld.com>
2018-10-10 06:52:03 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -4,17 +4,21 @@ import React from 'react';
 export default class DigitalClicker extends React.Component {
   constructor() {
     super()
-    this.state.timesClicked = 0;
+    this.state = {
+      timesClicked: 0
+    }
 
   }
   handleClick = () => {
-   this.setState({
+   this.setState(
+     {
      timesClicked: this.state.timesClicked + 1
-   })
+   }
+ )
  }
   render() {
     return(
-      <button>Times clicked:{this.state.timesClicked}</button>
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
     )
   }
 }
